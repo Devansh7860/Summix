f32fe0cd63abea5d7f62d84379f33135a453536a
Devansh Bansal <devanshb7860@gmail.com>
2025-09-23 13:00:50 +0530

    Fix playwright install command again

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
+#!/usr/bin/env bash
+# exit on error
 set -e
 
 pip install -r requirements.txt
-python -m playwright install --with-deps chromium
+python -m playwright install chromium
